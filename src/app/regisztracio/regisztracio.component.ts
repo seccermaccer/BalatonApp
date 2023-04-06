31ab62bbe3488a5815194c6061eb36243ee50c22
Kovács Sándor <sandor.kovacs@inclouded.hu>
2023-04-06 14:38:32 +0200

    user

--- src/app/regisztracio/regisztracio.component.ts
+++ src/app/regisztracio/regisztracio.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../shared/services/auth.service";
 import {Router} from "@angular/router";
+import {User} from "../shared/models/User";
+import {UserService} from "../shared/services/user.service";
 
 @Component({
   selector: 'app-regisztracio',
@@ -12,11 +14,14 @@ export class RegisztracioComponent implements OnInit {
 
   registerForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,private authS: AuthService,private router: Router) {
+  constructor(private formBuilder: FormBuilder,private authS: AuthService,private router: Router,private userS: UserService) {
     this.registerForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required]
+
     },{ validators: this.passwordMatchValidator });
   }
 
@@ -27,13 +32,26 @@ export class RegisztracioComponent implements OnInit {
     if (this.registerForm.valid) {
       console.log(this.registerForm.value);
       const { email, password } = this.registerForm.value;
-      this.authS.signUp(email,password).then(() => {
+      this.authS.signUp(email,password).then(cred => {
         console.log("Sikeres regisztráció");
         window.alert("Sikeres regisztráció!");
         this.router.navigate(['osszes']);
 
+        const user: User = {
+          id: cred.user?.uid as string,
+          email: this.registerForm.get('email')?.value,
+          username: this.registerForm.get('email')?.value.split('@')[0],
+          name: {
+            firstname: this.registerForm.get('firstname')?.value,
+            lastname: this.registerForm.get('lastname')?.value
+          }
+        };
 
-
+        this.userS.create(user).then(_ => {
+          console.log("Sikeres adatfelvitel!");
+        }).catch(error => {
+          console.error(error)
+        })
 
       }).catch(error => {
         console.error(error);
